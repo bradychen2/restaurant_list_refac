01ad28609be8735eedc9d99a1cbab4e710f181c8
bradychen <bradychen2@gmail.com>
2021-01-31 19:46:53 +0800

    fix: value in edit page does not show after space

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,14 +3,17 @@ const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   name: {
     type: String,
+    trim: false,
     required: true
   },
   name_en: {
     type: String,
+    trim: false,
     required: false
   },
   category: {
     type: String,
+    trim: false,
     required: false
   },
   rating: {
@@ -19,22 +22,27 @@ const restaurantSchema = new Schema({
   },
   location: {
     type: String,
+    trim: false,
     required: true
   },
   phone: {
     type: String,
+    trim: false,
     required: true
   },
   google_map: {
     type: String,
+    trim: false,
     required: false
   },
   image: {
     type: String,
+    trim: false,
     required: false
   },
   description: {
     type: String,
+    trim: false,
     required: false
   }
 })
